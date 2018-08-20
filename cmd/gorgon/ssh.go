75a7d4607166580ca78651feb126731750b74254
Tyler Booth <dru1d@dru1d.ninja>
2018-08-20 10:33:15 -0400

    removed extraneous lines

--- cmd/gorgon/ssh.go
+++ cmd/gorgon/ssh.go
@@ -98,22 +98,6 @@ type resp struct {
 }
 
 
-// func readLines(path string) ([]string, error) {
-// 	file, err := os.Open(path)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer file.Close()
-
-// 	var lines []string
-// 	scanner := bufio.NewScanner(file)
-// 	for scanner.Scan() {
-// 		lines = append(lines, scanner.Text())
-// 	}
-// 	return lines, scanner.Err()
-// }
-
-
 func sshAuth(username string, password string, host string, port string, timer int) *resp {
 	respond := &resp{}
 	clientConf := &ssh.ClientConfig {
